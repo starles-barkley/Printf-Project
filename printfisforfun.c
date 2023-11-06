023f2aa0996d878ff5a56d60d0cabfec31f49c12
NolbsAtHolbs <144157130+NolbsAtHolbs@users.noreply.github.com>
2023-11-06 23:52:04 +0000

    made beta printfisforfun.c file

---
+++ printfisforfun.c
@@ -0,0 +1,14 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <stddef.h>
+#include <stdarg.h>
+#include "main.h"
+/**
+ * printfisforfun - function that produces output according to a format
+ * @format: character string that handles conversion specifiers
+ * Return: void
+ */
+void printfisforfun(const char *format, ...)
+{
+
+}
