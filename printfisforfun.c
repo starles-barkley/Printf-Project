ef531cb0d8c283254becc716fa048675f216d877
starles-barkley <144153159+starles-barkley@users.noreply.github.com>
2023-11-07 00:42:34 +0000

    add section to process format string

--- printfisforfun.c
+++ printfisforfun.c
@@ -10,5 +10,38 @@
  */
 void printfisforfun(const char *format, ...)
 {
+	va_list args;
+	va_start(args, format);
+
+	int count = 0;
+
+	while (*format != '\0')
+	{
+		if (*format != '%')
+		{
+			putchar(*format);
+			count++;
+		}
+		else 
+		{
+			format++;
+		}
+			if (*format == 'c')
+			{	
+				int character = va_arg(args, int);
+				putchar(character);
+				count++;
+			}
+			else if (*format == 's')
+			{
+				const char *str = va_arg(args, const char *)
+			}
+				while (*str != '\0')
+				{
+					putchar(*str);
+					count++;
+					str++;
+				}
+
 
 }
