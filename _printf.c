d627fb1ad1a3e7095d431dc747e3467d7947c547
starles-barkley <144153159+starles-barkley@users.noreply.github.com>
2023-11-14 23:47:27 +0000

    fix?

--- _printf.c
+++ _printf.c
@@ -1,64 +1,86 @@
 #include <stdio.h>
+#include <stdlib.h>
+#include <stddef.h>
 #include <stdarg.h>
 #include "main.h"
-
-int _printf(const char *format, ...) {
-    if (format == NULL) {
-        return -1; // Handle NULL format string
-    }
-
-    va_list args;
+/**
+ * _printf - function that produces output according to a format
+ * @format: character string that handles conversion specifiers
+ * Return: number of characters printed
+ */
+int _printf(const char *format, ...)
+{
+    va_list args; /* declared variable of type va_list to hold variable arguments */
     int count = 0;
+    char *p;
+    char *sp; /* declared pointer to current character in string arg */
+    char *bp; /* declared pointer to current character in buffer */
+    char buffer[100];
 
     va_start(args, format);
-    while (*format != '\0') {
-        if (*format != '%') {
-            putchar(*format);
-            count++;
-        } else {
-            format++;
-            if (*format == '\0') {
-                break; // Format ends with '%', handle it as needed
-            }
+    p = (char *)format;
 
-            switch (*format) {
-                case 'c': {
-                    char c = va_arg(args, int);
-                    putchar(c);
+    while (*p != '\0')
+    {
+        if (*p != '%')
+        {
+            _putchar(*p);
+            count++;
+        }
+        else
+        {
+            p++;
+
+            if (*p == '\0')
+            {
+                _putchar('%');
+                count++;
+                break;
+            }
+            if (*p == 'c')
+            {
+                char c = va_arg(args, int);
+                _putchar(c);
+                count++;
+            }
+            else if (*p == 's')
+            {
+                char *s = va_arg(args, char *);
+                sp = s;
+
+                while (*sp != '\0')
+                {
+                    _putchar(*sp);
                     count++;
-                    break;
+                    sp++;
                 }
-                case 's': {
-                    char *s = va_arg(args, char *);
-                    if (s != NULL) {
-                        while (*s != '\0') {
-                            putchar(*s);
-                            s++;
-                            count++;
-                        }
-                    } else {
-                        // Handle NULL string pointer
-                        printf("(null)");
-                        count += 6;
-                    }
-                    break;
-                }
-                case '%': {
-                    putchar('%');
+            }
+            else if (*p == '%')
+            {
+                _putchar('%');
+                count++;
+            }
+            else if (*p == 'd' || *p == 'i')
+            {
+                int i = va_arg(args, int);
+                sprintf(buffer, "%d", i);
+                bp = buffer;
+                while (*bp != '\0')
+                {
+                    _putchar(*bp);
                     count++;
-                    break;
+                    bp++;
                 }
-                default: {
-                    // Handle unsupported format specifier
-                    putchar('%');
-                    putchar(*format);
-                    count += 2;
-                    break;
-                }
+            }
+            else
+            {
+                _putchar('%');
+                _putchar(*p);
+                count += 2;
             }
         }
-        format++;
+        p++;
     }
     va_end(args);
-    return count;
+    return (count);
 }
